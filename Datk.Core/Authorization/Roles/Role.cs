226e82b1f9644cdfaa5cb1259af5d744a2e4b118
minhanh0610 <leminhanh0610@gmail.com>
2022-07-06 14:08:11 +0700

    add file role

---
+++ Datk.Core/Authorization/Roles/Role.cs
@@ -0,0 +1,35 @@
+﻿using Abp.Authorization.Roles;
+using Datk.Authorization.Users;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datk.Authorization.Roles
+{
+    public class Role : AbpRole<User>
+    {
+        public const int MaxDescriptionLength = 5000;
+
+        public Role()
+        {
+        }
+
+        public Role(int? tenantId, string displayName)
+            : base(tenantId, displayName)
+        {
+
+        }
+
+        public Role(int? tenantId, string name, string displayName)
+            : base(tenantId, name, displayName)
+        {
+
+        }
+
+        [MaxLength(MaxDescriptionLength)]
+        public string Description { get; set; }
+    }
+}
